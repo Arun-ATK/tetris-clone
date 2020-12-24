cbb580aba9a7a981379f95db3ae796fccc4e6281
Arun-ATK <61749361+Arun-ATK@users.noreply.github.com>
2020-12-24 18:49:22 +0530

    initial commit

---
+++ Scripts/Cell.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public float fallTimer = 1f;
+    public GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= fallTimer) {
+            Fall();
+
+            fallTimer = Time.time + 1;
+        }
+    }
+
+    void Fall()
+    {
+        int x = (int) transform.position.x;
+        int y = (int) transform.position.y;
+
+        if (y <= 0) {
+            return;
+        }
+
+        if (!gm.playField[x,y - 1]) {
+            transform.position = new Vector2(x, y - 1);
+        }
+    }
+}
---
+++ Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool[,] playField = new bool[10, 22];
+    public Transform[] spawnerPoints;
+
+    public GameObject tetrominos;
+
+    //public GameObject cell;
+    //public Transform[] spawnPoints;
+    //[Header("Tetrominos")]
+    //int[] tetOSpawnPoints = { 0, 1, 3, 4 };
+
+    private void Start()
+    {
+        GameObject tetO = tetrominos.transform.GetChild(0).gameObject;
+
+        //foreach (var spPoint in tetO) {
+        //    Instantiate(cell, spawnPoints[spPoint].position, Quaternion.identity);
+        //}
+
+        
+    }
+}
