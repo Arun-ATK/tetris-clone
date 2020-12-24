df494e6d7cb7f7bb5d4b2e958d6dc2342cbd86af
Arun-ATK <61749361+Arun-ATK@users.noreply.github.com>
2020-12-24 19:24:28 +0530

    Added all tetromino prefabs

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -5,8 +5,9 @@ public class GameManager : MonoBehaviour
     public bool[,] playField = new bool[10, 22];
     public Transform[] spawnerPoints;
 
-    public GameObject tetrominos;
+    public GameObject[] tetrominos;
 
+    //public GameObject tetrominos;s
     //public GameObject cell;
     //public Transform[] spawnPoints;
     //[Header("Tetrominos")]
@@ -14,12 +15,13 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        GameObject tetO = tetrominos.transform.GetChild(0).gameObject;
-
+        //GameObject tetO = tetrominos.transform.GetChild(3).gameObject;
         //foreach (var spPoint in tetO) {
         //    Instantiate(cell, spawnPoints[spPoint].position, Quaternion.identity);
         //}
 
-        
+        GameObject tetO = tetrominos[3].transform.gameObject;
+        Instantiate(tetO, spawnerPoints[1].position, Quaternion.identity);
+
     }
 }
